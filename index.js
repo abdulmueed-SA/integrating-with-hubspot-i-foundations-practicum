983ef45bf665903002de0554f40935ac5c0b81e2
Abdul Mueed <abdul.mueed@workabot.ai>
2026-06-02 11:35:06 +0500

    Completed HubSpot custom object integration

--- index.js
+++ index.js
@@ -8,18 +8,83 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config();
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
+app.get('/', async (req, res) => {
 
+const movies =
+'https://api.hubapi.com/crm/v3/objects/2-63442938?properties=name,director,genre';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+
+        const response = await axios.get(movies, { headers });
+
+        const data = response.data.results;
+        
+        console.log(JSON.stringify(data, null, 2));
+
+        res.render('homepage', {
+            title: 'Movies',
+            data
+        });
+
+    } catch (error) {
+
+        console.error(error.response?.data || error.message);
+
+        res.send('Error retrieving movies');
+
+    }
+
+});
 // * Code for Route 1 goes here
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update-cobj', (req, res) => {
 
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+
+});
 // * Code for Route 2 goes here
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
 
+    const createMovie = {
+        properties: {
+            name: req.body.name,
+            director: req.body.director,
+            genre: req.body.genre
+        }
+    };
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+
+        await axios.post(
+            'https://api.hubapi.com/crm/v3/objects/2-63442938',
+            createMovie,
+            { headers }
+        );
+
+        res.redirect('/');
+
+    } catch (error) {
+
+        console.error(error.response?.data || error);
+
+    }
+
+});
 // * Code for Route 3 goes here
 
 /** 
